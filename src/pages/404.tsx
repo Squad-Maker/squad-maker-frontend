46c2907e6c1973253ce711e57e064c0bbe36ea81
Vinicius <vinix464@gmail.com>
2024-11-13 13:14:54 -0300

    fix: Correção erros eslint

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,29 @@
 import { Link } from 'react-router-dom'
+
+import error404 from '@/assets/error404.svg'
+import error404Dark from '@/assets/error404Dark.svg'
+import { useTheme } from '@/components/theme/theme-provider'
 import { Button } from '@/components/ui/button'
 
-import { useTheme } from '@/components/theme/theme-provider';
-
-import error404 from '../assets/error404.svg';
-import error404Dark from '../assets/error404Dark.svg';
-
 export function NotFound() {
-  const { theme } = useTheme();
-  
+  const { theme } = useTheme()
+
   return (
     <div className="flex h-screen flex-col sm:flex-row bg-background items-center justify-center gap-2 space-y-4">
-      <div className='flex items-center justify-center px-4'>
-        <img className='w-[75%] sm:w-full items-center' src={theme == 'light' ? error404Dark : error404} alt="Imagem erro 404" /> 
+      <div className="flex items-center justify-center px-4">
+        <img
+          className="w-[75%] sm:w-full items-center"
+          src={theme === 'light' ? error404Dark : error404}
+          alt="Imagem erro 404"
+        />
       </div>
-      <div className='px-9 text-center sm:text-left'>
-        <p className="mb-4 text-xl font-bold md:text-3xl">Houston, temos um problema!</p>
+      <div className="px-9 text-center sm:text-left">
+        <p className="mb-4 text-xl font-bold md:text-3xl">
+          Houston, temos um problema!
+        </p>
         <p className="mb-4 text-lg font-normal text-muted-foreground">
-          Parece que esta página foi engolida por um buraco negro. Tente outra rota!
+          Parece que esta página foi engolida por um buraco negro. Tente outra
+          rota!
         </p>
         <Button asChild>
           <Link to="/">Voltar à página inicial</Link>
--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,31 +1,38 @@
 import { Link, useRouteError } from 'react-router-dom'
 
+import errorImg from '@/assets/error.svg'
+import errorDark from '@/assets/errorDark.svg'
+import { useTheme } from '@/components/theme/theme-provider'
 import { Button } from '@/components/ui/button'
-import { useTheme } from '@/components/theme/theme-provider';
-
-import errorDark from '../assets/errorDark.svg';
-import errorImg from '../assets/error.svg';
 
 export function Error() {
   const error = useRouteError() as Error
-  const { theme } = useTheme();
+  const { theme } = useTheme()
 
   return (
     <div className="flex h-screen flex-col sm:flex-row bg-background items-center justify-center gap-2 space-y-4">
-      <div className='flex items-center justify-center px-4'>
-        <img className='w-[75%] sm:w-full items-center' src={theme == 'light' ? errorImg : errorDark} alt="Imagem erro 404" /> 
+      <div className="flex items-center justify-center px-4">
+        <img
+          className="w-[75%] sm:w-full items-center"
+          src={theme === 'light' ? errorImg : errorDark}
+          alt="Imagem erro 404"
+        />
       </div>
-      <div className='px-9 text-center sm:text-left'>
-        <p className="mb-4 text-xl font-bold md:text-3xl">Houston, estamos fora de órbita!</p>
-        <p className="mb-4 text-lg font-normal text-muted-foreground">
-          Parece que esta página teve alguns problemas, veja com mais detalhes abaixo:
+      <div className="px-9 text-center sm:text-left">
+        <p className="mb-4 text-xl font-bold md:text-3xl">
+          Houston, estamos fora de órbita!
         </p>
-        <pre className="mb-4 text-lg font-normal text-muted-foreground">{error?.message || JSON.stringify(error)}</pre>
+        <p className="mb-4 text-lg font-normal text-muted-foreground">
+          Parece que esta página teve alguns problemas, veja com mais detalhes
+          abaixo:
+        </p>
+        <pre className="mb-4 text-lg font-normal text-muted-foreground">
+          {error?.message || JSON.stringify(error)}
+        </pre>
         <Button asChild>
           <Link to="/">Voltar à página inicial</Link>
         </Button>
       </div>
     </div>
-    
   )
 }
