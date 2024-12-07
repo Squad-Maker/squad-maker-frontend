72e51cbba10f0d52d2c501927122892a80b7f94c
Marcus Toledo <marcus_viniciustoledo@hotmail.com>
2024-12-07 18:35:06 -0300

    Update .commitlintrc.ts
    disable rule

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,7 +3,7 @@ import { UserConfig } from '@commitlint/types'
 const Configuration: UserConfig = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']],
+        'subject-case': [0, 'never', ['upper-case', 'pascal-case', 'start-case']],
     },
 }
 
