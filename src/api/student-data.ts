554ad05f1e626b8c8f2a3d7319a08214a9ee3e87
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2025-02-18 14:46:24 -0300

    fix: Remover envio de token inútil

--- src/api/student-data.ts
+++ src/api/student-data.ts
@@ -1,17 +1,7 @@
-import { BrowserHeaders } from 'browser-headers'
-
 import { squadServiceClient } from '@/lib/api'
 
 export async function fetchStudentData() {
-  const token = localStorage.getItem('accessToken')
-
-  if (!token) return
-
-  const metadata = new BrowserHeaders({
-    authorization: `Bearer ${token}`,
-  })
-
-  const response = await squadServiceClient.getStudentSubjectData({}, metadata)
+  const response = await squadServiceClient.getStudentSubjectData({})
 
   return response
 }
