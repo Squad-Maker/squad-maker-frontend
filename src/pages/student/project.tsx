38deaaa88ac1b0d6f6ecc14546aa416c7facde53
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2025-02-18 23:27:55 -0300

    feat: Validar cadastro do aluno

--- src/pages/student/project.tsx
+++ src/pages/student/project.tsx
@@ -1,7 +1,11 @@
-import { EllipsisVertical, Settings, Users } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { EllipsisVertical, EyeClosed, Settings, UserPen } from 'lucide-react'
+import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 
+import { fetchStudentData } from '@/api/student-data'
+import { fetchStudentProjects } from '@/api/stundent-projects'
 import ModalSaving from '@/components/modal-saving'
 import { Button } from '@/components/ui/button'
 import {
@@ -27,27 +31,23 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
-import { Project } from '@/grpc/generated/squad/project'
-import { squadServiceClient } from '@/lib/api'
 
 export function StudentProject() {
-  const [projects, setProjects] = useState<Project[]>([])
   const [openDialog, setOpenDialog] = useState(false)
   const [status, setStatus] = useState('loading')
   const [isOpen, setIsOpen] = useState(false)
 
-  async function loadProjects() {
-    const resp = squadServiceClient.readAllProjects({
-      pagination: {
-        limit: 100,
-      },
-    })
-    setProjects((await resp).data)
-  }
+  const { data: studentProjects = [] } = useQuery({
+    queryKey: ['studentProjects'],
+    queryFn: fetchStudentProjects,
+    retry: false,
+  })
 
-  useEffect(() => {
-    loadProjects()
-  }, [])
+  const { data: studentData } = useQuery({
+    queryKey: ['studentData'],
+    queryFn: fetchStudentData,
+    retry: false,
+  })
 
   function onSubmit() {
     console.log('Formulário enviado:')
@@ -68,37 +68,65 @@ export function StudentProject() {
     <>
       <Helmet title="Projetos" />
       <div className="px-12 py-4">
-        <p className="text-3xl font-semibold">Projetos</p>
+        <h1 className="text-2xl md:text-3xl pb-2 font-semibold">Projetos</h1>
+        <p className="text-muted-foreground">
+          Aqui você pode visualizar os projetos disponíveis para colaborar
+        </p>
 
-        <div className="space-y-4 p-8 ml-[1%] flex justify-around flex-wrap">
-          {projects.map((project, key) => (
+        {!studentData?.hadFirstUpdate ? (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <UserPen className="w-16 h-16 text-muted-foreground" />
+              <p className="text-muted-foreground text-lg mt-4 text-center">
+                Você precisa atualizar seu perfil para poder participar de um
+                projeto
+              </p>
+              <Button className="mt-4">
+                <Link to="/student/profile">Atualizar perfil</Link>
+              </Button>
+            </div>
+          </div>
+        ) : (
+          !studentProjects.length && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <EyeClosed className="w-16 h-16 text-muted-foreground" />
+              <p className="text-muted-foreground text-lg ml-4">
+                Você ainda não está em nenhum projeto
+              </p>
+            </div>
+          )
+        )}
+
+        <div className="grid grid-cols-2 gap-4 py-8">
+          {studentProjects.map((project, key) => (
             <div
               key={key}
-              className="border border-zinc-500 rounded-md shadow-xl dark:shadow-stone-800"
+              className="border border-input rounded-md p-4 h-full flex flex-col justify-between"
             >
-              <img
-                className="items-center rounded-md w-full"
-                src=""
-                alt="Imagem erro 404"
-              />
-              <div className="p-2">
-                <p className="font-medium">{project.name}</p>
-                <p className="font-light">{project.description}</p>
-              </div>
-              <div className="p-2">
+              <div className="flex justify-between items-start">
+                <div>
+                  <p className="font-medium pb-4 text-xl">{project.name}</p>
+                  <p>{project.description}</p>
+                  <div className="mt-4">
+                    <p className="font-bold text-sm">Colaboradores:</p>
+                    <div className="p-2">
+                      {project.students.map((student, index) => (
+                        <p key={index}>
+                          <span className="font-semibold">{student.name}</span>{' '}
+                          - {student.positionName}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
                 <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <EllipsisVertical color="gray" />
+                  <DropdownMenuTrigger className="focus:outline-none">
+                    <EllipsisVertical className="text-muted-foreground" />
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
                     <DropdownMenuItem onClick={() => setOpenDialog(true)}>
                       <Settings />
                       Reavaliação
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <Users />
-                      Chat
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
@@ -106,6 +134,7 @@ export function StudentProject() {
             </div>
           ))}
         </div>
+
         <Dialog open={openDialog} onOpenChange={setOpenDialog}>
           <DialogContent>
             <DialogHeader>
@@ -131,7 +160,7 @@ export function StudentProject() {
                     <SelectValue placeholder="" />
                   </SelectTrigger>
                   <SelectContent>
-                    {projects.map((project, key) => (
+                    {studentProjects.map((project, key) => (
                       <SelectItem key={key} value={project.id}>
                         {project.name}
                       </SelectItem>
