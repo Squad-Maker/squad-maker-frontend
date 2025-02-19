2149ca95ed9dba9843e3fc253de3e6a1ea49bd2e
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2025-02-19 13:14:41 -0300

    chore: remove regra do lint
    coloquei pra ficar como warning ao invés de erro

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,9 +1,10 @@
-import { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity, UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'subject-case': [0, 'never', ['upper-case', 'pascal-case', 'start-case']],
+        'subject-case': [RuleConfigSeverity.Disabled, 'never', ['upper-case', 'pascal-case', 'start-case']],
+        "body-max-line-length": [RuleConfigSeverity.Warning, "always", 100],
     },
 }
 
