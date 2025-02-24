31d0cf9d1f6c037e9cbab0b68ba4ee0d1ce6b4bf
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2025-02-24 20:42:07 -0300

    fix: correção de erros do form de time

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,8 +1,17 @@
+import { Order } from '@/grpc/generated/common/pagination'
 import { squadServiceClient } from '@/lib/api'
 
 export async function fetchProjects() {
   const response = await squadServiceClient.readAllProjects({
-    pagination: { limit: 100 },
+    pagination: {
+      limit: 100,
+      sortOptions: [
+        {
+          order: Order.ordAscending,
+          sort: 'id',
+        },
+      ],
+    },
   })
 
   return response.data
